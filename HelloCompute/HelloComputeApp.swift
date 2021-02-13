c229483c1ebcef089d00c4f52a4c245a45f6b46a
Shogo <vanitas710@yahoo.co.jp>
2021-02-14 02:35:16 +0900

    Initial Commit

---
+++ HelloCompute/HelloComputeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloComputeApp.swift
+//  HelloCompute
+//
+//  Created by Shogo Nobuhara on 2021/02/14.
+//
+
+import SwiftUI
+
+@main
+struct HelloComputeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
